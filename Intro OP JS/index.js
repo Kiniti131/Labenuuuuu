b1317b992ba4e79b0678745a167c4a3b534ffb95
kiniti131 <kiniti131@gmail.com>
2022-11-23 09:03:09 -0300

    aula 2 de js

---
+++ Intro OP JS/index.js
@@ -0,0 +1,21 @@
+console.log('Operadores')
+
+let saldo = 15;
+
+     exemplo1 = saldo + 22.4;
+        console.log(exemplo1);
+
+     exemplo2 = exemplo1 - 12.34;   
+        console.log(exemplo2)   
+    
+    exemplo3 = exemplo2/3;
+        console.log(exemplo3);
+
+    exemplo4 = exemplo3 * 3.42;
+        console.log(exemplo4);
+    
+    exemplo5 =parseInt(exemplo4/3);
+        exemplo6 = exemplo4-exemplo5*3;
+
+        console.log(exemplo5);
+        console.log(exemplo6);    
